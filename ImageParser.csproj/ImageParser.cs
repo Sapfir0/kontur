97d88ca5264afacc18d62b760313df5bf4369d81
Sapfir0 <sapfir999999@yandex.ru>
2019-10-30 20:18:23 +0400

    fix

--- ImageParser.csproj/ImageParser.cs
+++ ImageParser.csproj/ImageParser.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ImageParser
 {
@@ -9,23 +10,49 @@ namespace ImageParser
         public string GetImageInfo(Stream stream) {
             Console.WriteLine("Я тут");
             long imageSize = stream.Length;
-            List<String> header = new List<String>();
-
-            for (int i = 0; i < 8; i++){
-                String hex = string.Format("{0:X2}", stream.ReadByte());
+            List<string> header = new List<string>();
+            int headerIsEnd = 8; // самый длинный хедер
+            for (int i = 0; i < headerIsEnd; i++){
+                string hex = $"{stream.ReadByte():X2}";
                 header.Add(hex);
             }
-            var result = String.Join("", header.ToArray());
-            if (result.Contains("89504E470D0A1A0A"))  {
+            var result = string.Join("", header.ToArray());
+            const string pngMarker = "89504E470D0A1A0A";
+            const string gifMarker = "474946";
+            const string bmpMarker = "424d0a";
+            if (result.Contains(pngMarker))  {
                 Console.Write("Png");
             }
-            else if (result.Contains("474946"))  {
+            else if (result.Contains(gifMarker))  {
                 Console.Write("GIF");
             }
-            else if(result.Contains("424d0a"))    {
+            else if(result.Contains(bmpMarker)) {
                 Console.Write("BMP");
             }
             
+            List<string> hexTest = new List<string>();
+            string IHDRmarker = "49484452";
+            bool IHDRstarted = false;
+            int count = 0;
+            List<string> sizeParams = new List<string>(8);
+            for (int i = 8; i < 24; i++){
+                string hex = $"{stream.ReadByte():X2}";
+                hexTest.Append(hex);
+                string hexString = string.Join("", hexTest.ToArray());
+                Console.Write(hexString);
+                if (hexString.Contains(IHDRmarker))  {
+                    IHDRstarted = true;
+                }
+                if (IHDRstarted && count < 8)  {
+                    sizeParams.Append(hex);
+                    count += 1;
+                }
+            }
+
+            foreach (var VARIABLE in sizeParams)
+            {
+                Console.WriteLine(VARIABLE);                
+            }
             
             //System.Drawing.Image image = System.Drawing.Image.FromStream(stream);
             //image.Height.ToString(); 

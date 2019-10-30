6128ce7718a4654d48b4e3870003c2495ee9f492
Sapfir0 <sapfir999999@yandex.ru>
2019-10-30 21:46:53 +0400

    png

--- ImageParser.csproj/ImageParser.cs
+++ ImageParser.csproj/ImageParser.cs
@@ -2,14 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
+
 
 namespace ImageParser
 {
     public class ImageParser : IImageParser
     {
-        public string GetImageInfo(Stream stream) {
-            Console.WriteLine("Я тут");
-            long imageSize = stream.Length;
+        private string detectImageType(Stream stream) {
             List<string> header = new List<string>();
             int headerIsEnd = 8; // самый длинный хедер
             for (int i = 0; i < headerIsEnd; i++){
@@ -21,25 +21,34 @@ namespace ImageParser
             const string gifMarker = "474946";
             const string bmpMarker = "424d0a";
             if (result.Contains(pngMarker))  {
-                Console.Write("Png");
+                return "Png";
             }
             else if (result.Contains(gifMarker))  {
-                Console.Write("GIF");
+                return "Gif";
             }
-            else if(result.Contains(bmpMarker)) {
-                Console.Write("BMP");
+            else if(result.Contains(bmpMarker))  {
+                return "Bmp";
             }
-            
+            else  {
+                return "Undefined format";
+            }
+        }
+
+        private long getImageSize(Stream stream)  {
+            return stream.Length;
+        }
+
+        private List<int> getImageWidthHeight(Stream stream) {
             List<string> hexTest = new List<string>();
-            string IHDRmarker = "49484452";
+            const string IHDRmarker = "49484452";
             bool IHDRstarted = false;
             int count = 0;
             List<string> sizeParams = new List<string>(8);
             for (int i = 8; i < 24; i++){
                 string hex = $"{stream.ReadByte():X2}";
                 hexTest.Append(hex);
-                string hexString = string.Join("", hexTest.ToArray());
-                Console.Write(hexString);
+                string hexString = "kkk";//string.Join("", hexTest.ToArray());
+                
                 if (hexString.Contains(IHDRmarker))  {
                     IHDRstarted = true;
                 }
@@ -48,17 +57,26 @@ namespace ImageParser
                     count += 1;
                 }
             }
+            List<int> size = new List<int>(2);
+            size[0] = 0;
+            size[1] = 1;
+            return size;
+        }
 
-            foreach (var VARIABLE in sizeParams)
-            {
-                Console.WriteLine(VARIABLE);                
-            }
-            
-            //System.Drawing.Image image = System.Drawing.Image.FromStream(stream);
-            //image.Height.ToString(); 
-            //image.Width.ToString();
-
-            return "s";
+        private class ImageInfo {
+            //public int Height;
+            //public int Width;
+            public string Format;
+            public long Size;
+        }
+        
+        public string GetImageInfo(Stream stream) {
+            Console.WriteLine("Я тут");
+            ImageInfo imgInfo = new ImageInfo();
+            imgInfo.Format = detectImageType(stream);
+            imgInfo.Size = getImageSize(stream);
+            string json = JsonConvert.SerializeObject(imgInfo);
+            return json;
         }
     }
 }

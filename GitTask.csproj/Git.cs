7ca120801070c2d919b7b6324818adaa348ea01d
Sapfir0 <sapfir999999@yandex.ru>
2019-10-30 18:56:35 +0400

    foo

--- GitTask.csproj/Git.cs
+++ GitTask.csproj/Git.cs
@@ -12,14 +12,14 @@ namespace GitTask
     {
         private Dictionary<int, Dictionary<int, string>> gitTree = new Dictionary<int, Dictionary<int, string>>(); // {номерКоммита: {номерФайла: содеримоеФайла}}
         public Git(int filesCount) {
-            Console.Write("конструктор");
             for (int i = 0; i < filesCount; i++) {
-                string filepath = "./" + i;
+                string filepath = "./" + i + ".txt";
                 FileStream fs = File.Create(filepath);
+                fs.Close();
             }
         }
         public void Update(int fileNumber, int value) {
-            string filepath = "./" + fileNumber;
+            string filepath = "./" + fileNumber + ".txt";
             using (FileStream fs = File.Open(filepath, FileMode.Open, FileAccess.Write)) {
                 Byte[] info = new UTF8Encoding(true).GetBytes(value.ToString());
                 fs.Write(info, 0, info.Length);
@@ -29,10 +29,18 @@ namespace GitTask
             FileInfo[] listdir = new DirectoryInfo(@"./").GetFiles();
             Dictionary<int, string> filesDescription = new Dictionary<int, string>(listdir.Length); // название файла : содержимое файла
             
-            foreach (var file in listdir)  {
-                Console.Write(file.Name);
-                string[] readText = File.ReadAllLines(file.Name); //если предположить по примеру что заполнятеся только верхняя строка, то берем просто первый элемент 
-                filesDescription.Add(Convert.ToInt32(file.Name), readText[0]);
+            foreach (var file in listdir) {
+                if (new FileInfo(file.Name).Length == 0)
+                {
+                    Console.WriteLine("Пытаемся считать пустой файл");
+                    return -1;
+                }
+                string textContent = File.ReadLines(file.Name).First();    //если предположить по примеру что заполнятеся только верхняя строка, то берем просто первый элемент 
+
+                string withoutExtension = file.Name.Split('.')[0];
+                Console.WriteLine(file.Name, withoutExtension);
+                Console.WriteLine(withoutExtension);
+                filesDescription.Add(Convert.ToInt32(withoutExtension), textContent);
             }
 
             gitTree.Add(gitTree.Count + 1, filesDescription);
@@ -41,8 +49,10 @@ namespace GitTask
         }
 
         public int Checkout(int commitNumber, int fileNumber) {
-            Dictionary<int, string> ourCommit = gitTree.Keys.ElementAt(commitNumber); // название файла : содержимое файла
-            return ourCommit[fileNumber];
+            Console.WriteLine(gitTree.Values.Count);
+            Dictionary<int, string> ourCommit = gitTree.Values.ElementAt(commitNumber); // название файла : содержимое файла
+            int fileContent = ourCommit.Keys.ElementAt(fileNumber);
+            return fileContent;
 
         }
     }
